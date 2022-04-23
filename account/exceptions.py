9b5fc890521075e992ec0b73d293901375031d2e
taxijjang <gw9122@naver.com>
2022-04-23 15:47:33 +0900

    회원가입시 비밀번호 오입력을 막기 위해 검증 비밀번호 추가

--- account/exceptions.py
+++ account/exceptions.py
@@ -5,3 +5,9 @@ class SMSAuthIsExpiredException(APIException):
     status_code = 400
     default_detail = "문자 인증이 완료된 이후에 회원가입이 가능합니다."
     default_code = "MUST_SMS_AUTH"
+
+
+class PasswordIsNotValidException(APIException):
+    status_code = 400
+    default_detail = "입력한 비밀번호가 다릅니다."
+    default_code = "PASSWORD_IS_NOT_VALID"
--- account/serializers/sign_up.py
+++ account/serializers/sign_up.py
@@ -3,15 +3,25 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from sms.models import AuthSMS
-from ..exceptions import SMSAuthIsExpiredException
+from ..exceptions import SMSAuthIsExpiredException, PasswordIsNotValidException
 
 User = get_user_model()
 
 
 class SignUpSerializer(serializers.ModelSerializer):
+    verify_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ("id", "email", "password", "name", "nickname", "phone_number")
+        fields = (
+            "id",
+            "email",
+            "password",
+            "verify_password",
+            "name",
+            "nickname",
+            "phone_number",
+        )
         extra_kwargs = {
             "password": {"write_only": True},
         }
@@ -23,6 +33,14 @@ class SignUpSerializer(serializers.ModelSerializer):
             raise SMSAuthIsExpiredException()
         return phone_number
 
+    def validate(self, attrs):
+        password = attrs.get("password")
+        verify_password = attrs.get("verify_password")
+        if password != verify_password:
+            raise PasswordIsNotValidException()
+        return attrs
+
     def create(self, validated_data):
+        _ = validated_data.pop("verify_password")
         user = User.objects.create_user(**validated_data)
         return user

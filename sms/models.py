faaeb6fd15172a73e9d8a1cb58ca6cb8b0bb5e28
taxijjang <gw9122@naver.com>
2022-04-23 12:56:49 +0900

    문자인증시 타임아웃을 제한하는 코드 작성 추가

--- sms/models.py
+++ sms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from core.models import BaseTimestampModel
 
@@ -8,6 +9,7 @@ class AuthSMS(BaseTimestampModel):
     TYPES_SMS_AUTH_CHOICES = [
         (SIGN_UP, "sign_up"),
     ]
+    VERIFY_TIME = 5  # 5minute
     phone_number = models.CharField(db_index=True, max_length=11)
     code = models.CharField(db_index=True, max_length=6)
     verify = models.DateTimeField(db_index=True, null=True, blank=True)
@@ -15,3 +17,14 @@ class AuthSMS(BaseTimestampModel):
 
     class Meta:
         ordering = ["-pk"]
+
+    @classmethod
+    def is_valid_sms_auth(cls, phone_number):
+        queryset = cls.objects.filter(phone_number=phone_number)
+        queryset = queryset.filter(verify__isnull=True)
+        queryset = queryset.filter(
+            verify__gte=timezone.now() - timezone.timedelta(minutes=cls.VERIFY_TIME)
+        )
+        if queryset.exists():
+            return True
+        return False

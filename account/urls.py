397f1cf8412e802fb772b8bdab6b19c9c32589cb
taxijjang <gw9122@naver.com>
2022-04-23 15:23:23 +0900

    토큰 갱신하는 주소 변경

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="회원가입"),
     path("signin/", views.SignInView.as_view(), name="로그인"),
+    path("refresh/", TokenRefreshView.as_view(), name="JWT 토큰 갱신"),
 ]

a6eae67c5c6b5b5e6ff398dc6d37bc28118480e2
taxijjang <gw9122@naver.com>
2022-04-23 16:58:09 +0900

    문자 인증 반환 메시지 변경

--- account/exceptions.py
+++ account/exceptions.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import APIException
 
 class SMSAuthIsExpiredException(APIException):
     status_code = 400
-    default_detail = "문자 인증이 완료된 이후에 회원가입이 가능합니다."
+    default_detail = "문자 인증이 완료되어야 합니다."
     default_code = "MUST_SMS_AUTH"
 
 
